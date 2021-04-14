b41865472159964680c0a4fdba7d01a5a8c6de21
KenichiFujita <50497605+KenichiFujita@users.noreply.github.com>
2021-04-14 01:00:36 -0400

    Tab bar (#3)

--- HNBUI/TabBar.swift
+++ HNBUI/TabBar.swift
@@ -7,17 +7,163 @@
 
 import UIKit
 
+public protocol TabBarDelegate: AnyObject {
+
+    func tabBar(_ tabBar: TabBar, didSelectItemAtIndex index: Int)
+
+}
+
 public final class TabBar: UIView {
+
+    public weak var delegate: TabBarDelegate?
+
+    public var items: [UITabBarItem] = [] {
+        didSet {
+            configure()
+        }
+    }
+
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.isScrollEnabled = true
+        scrollView.showsHorizontalScrollIndicator = false
+        return scrollView
+    }()
+
+    private let hStack: UIStackView = {
+        let hStack = UIStackView()
+        hStack.translatesAutoresizingMaskIntoConstraints = false
+        hStack.axis = .horizontal
+        hStack.distribution = .fill
+        hStack.alignment = .center
+        return hStack
+    }()
 
     public init() {
         super.init(frame: .zero)
+
+        backgroundColor = .systemBackground
+        addSubview(scrollView)
+        scrollView.addSubview(hStack)
+
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: self.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            hStack.topAnchor.constraint(equalTo: self.topAnchor),
+            hStack.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            hStack.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            hStack.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+        ])
+
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    private lazy var didTapTabBarItemCallback: (UITabBarItem) -> () = { [weak self] tabBarItem in
+        guard let strongSelf = self, let index = strongSelf.items.firstIndex(of: tabBarItem) else { return }
+        strongSelf.selectItem(at: index)
+    }
+
+    private func selectItem(at index: Int) {
+        delegate?.tabBar(self, didSelectItemAtIndex: index)
+        guard let tabBarItems = hStack.arrangedSubviews as? [TabBarItemView] else { return }
+        tabBarItems.forEach { tabBarItem in
+            tabBarItem.isSelected = false
+        }
+        tabBarItems[index].isSelected = true
+        let offset = hStack.arrangedSubviews[index].center.x + scrollView.layoutMargins.left - (bounds.width / 2)
+        let minimumOffset: CGFloat = 0
+        let maximumOffset = hStack.bounds.width - bounds.width
+        scrollView.setContentOffset(CGPoint(x: min(max(minimumOffset, offset), maximumOffset), y: 0), animated: true)
+    }
+
+    private func configure() {
+        hStack.subviews.forEach { view in
+            view.removeFromSuperview()
+        }
+        items.forEach { item in
+            let tabBarItem = TabBarItemView(item: item, didTapTabBarItemCallback: didTapTabBarItemCallback)
+            hStack.addArrangedSubview(tabBarItem)
+        }
+        if items.count > 0 {
+            selectItem(at: 0)
+        }
+    }
+
 }
 
 
+final class TabBarItemView: UIView {
 
+    private let item: UITabBarItem
+
+    var isSelected: Bool = false {
+        didSet {
+            updateColors()
+        }
+    }
+
+    private let itemImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .footnote)
+        return label
+    }()
+
+    private lazy var tapGesture: UITapGestureRecognizer = {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTap))
+        return tapGesture
+    }()
+
+    init(item: UITabBarItem, didTapTabBarItemCallback callback: @escaping (UITabBarItem) -> ()) {
+        self.item = item
+        super.init(frame: .zero)
+
+        addGestureRecognizer(tapGesture)
+        translatesAutoresizingMaskIntoConstraints = false
+        itemImageView.image = item.image
+        titleLabel.text = item.title
+        didTapTabBarItem = callback
+        updateColors()
+
+        addSubview(itemImageView)
+        addSubview(titleLabel)
+
+        NSLayoutConstraint.activate([
+            itemImageView.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
+            itemImageView.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor),
+            itemImageView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: layoutMargins.left),
+            itemImageView.trailingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: -(layoutMargins.left)),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor, constant: -(layoutMargins.right))
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    var didTapTabBarItem: (UITabBarItem) -> () = { _ in }
+
+    @objc private func didTap() {
+        didTapTabBarItem(item)
+    }
+
+    private func updateColors() {
+        titleLabel.textColor = isSelected ? .systemOrange : .secondaryLabel
+        itemImageView.tintColor = isSelected ? .systemOrange : .secondaryLabel
+    }
+
+}
--- HNBUIDemo/ViewController.swift
+++ HNBUIDemo/ViewController.swift
@@ -10,15 +10,52 @@ import HNBUI
 
 class ViewController: UIViewController {
 
+    private lazy var tabBar: TabBar = {
+        let tabBar = TabBar()
+        tabBar.translatesAutoresizingMaskIntoConstraints = false
+        tabBar.delegate = self
+        let item1 = UITabBarItem(title: "Top Story", image: UIImage(systemName: "list.number"), tag: 0)
+        let item2 = UITabBarItem(title: "Ask HN", image: UIImage(systemName: "questionmark.circle"), tag: 0)
+        let item3 = UITabBarItem(title: "Show HN", image: UIImage(systemName: "globe"), tag: 0)
+        let item4 = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 0)
+        let item5 = UITabBarItem(title: "Favorite", image: UIImage(systemName: "star"), tag: 0)
+        tabBar.items = [item1, item2, item3, item4, item5]
+        return tabBar
+    }()
+
+    private let demoLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .systemBackground
+        label.textAlignment = .center
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
-
-        let tabBar = TabBar()
         view.addSubview(tabBar)
-    }
+        view.addSubview(demoLabel)
 
+        NSLayoutConstraint.activate( [
+            tabBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tabBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tabBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            
+            demoLabel.heightAnchor.constraint(equalToConstant: 100),
+            demoLabel.widthAnchor.constraint(equalToConstant: 100),
+            demoLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            demoLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+
+    }
 
 }
 
+extension ViewController: TabBarDelegate {
+
+    func tabBar(_ tabBar: TabBar, didSelectItemAtIndex index: Int) {
+        demoLabel.text = String(index)
+    }
+
+}
