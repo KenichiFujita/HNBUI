b785810550d84e4f95d9ae46088baf25f62d2cec
Ken <ken.fujita1130@icloud.com>
2021-08-24 22:59:55 -0400

    Add blur effect. Revise demo.

--- HNBUIDemo/TabBarViewController.swift
+++ HNBUIDemo/TabBarViewController.swift
@@ -9,34 +9,36 @@ import UIKit
 import HNBUI
 
 class TabBarViewController: TabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
-
+        
         let topStoriesViewController = ViewController(tabBarItemTitle: "Top Stories",
                                                       tabBarItemImage: nil,
                                                       backgroundColor: .blue)
-        
-
+        let askHNStoriesViewController = ViewController(tabBarItemTitle: "Ask HN",
+                                                        tabBarItemImage: nil,
+                                                        backgroundColor: .brown)
+        let showHNStoriesViewController = ViewController(tabBarItemTitle: "Show HN",
+                                                         tabBarItemImage: nil,
+                                                         backgroundColor: .cyan)
+        let searchStoriesViewController = ViewController(tabBarItemTitle: "Search",
+                                                         tabBarItemImage: nil,
+                                                         backgroundColor: .darkGray)
+        let favoritesStoriesViewController = ViewController(tabBarItemTitle: "Favorite",
+                                                            tabBarItemImage: nil,
+                                                            backgroundColor: .green)
         viewControllers = [
             topStoriesViewController,
-            viewController(tabBarItemTitle: "Ask HN",
-                           tabBarItemImage: nil,
-                           backgroundColor: .brown),
-            viewController(tabBarItemTitle: "Show HN",
-                           tabBarItemImage: nil,
-                           backgroundColor: .cyan),
-            viewController(tabBarItemTitle: "Search",
-                           tabBarItemImage: nil,
-                           backgroundColor: .darkGray),
-            viewController(tabBarItemTitle: "Favorite",
-                           tabBarItemImage: nil,
-                           backgroundColor: .green)
+            askHNStoriesViewController,
+            showHNStoriesViewController,
+            searchStoriesViewController,
+            favoritesStoriesViewController
         ]
     }
-
+    
     func viewController(tabBarItemTitle: String, tabBarItemImage: UIImage?, backgroundColor: UIColor) -> UIViewController {
         let viewController = UIViewController()
         viewController.tabBarItem.title = tabBarItemTitle
@@ -44,5 +46,5 @@ class TabBarViewController: TabBarController {
         viewController.view.backgroundColor = backgroundColor
         return viewController
     }
-
+    
 }
